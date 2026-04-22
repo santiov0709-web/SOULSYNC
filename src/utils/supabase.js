341b83e9004fb0e3cfe4cfe7bac4549ee9206007
Santiago <santi@example.com>
2026-04-22 01:04:36 -0500

    Fix Supabase URL

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://wxwtxwkctkzjktpgufzc.supabase.co';
+const supabaseUrl = 'https://wxwtxwkotkzjktpgufzc.supabase.co';
 // Si esta key falla porque la plataforma ha cambiado el formato (normalmente empiezan con eyJ), el usuario la podrá actualizar.
 const supabaseKey = 'sb_publishable_9HMh7EhnSiwh120rz1qw8g_Y5aqxR9Y';
 
